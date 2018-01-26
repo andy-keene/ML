499bbe284132b56c38f0fe19cfa761bbf7fc9acd
andy-keene <keene.andy.t@gmail.com>
2018-01-25 19:19:48 -0800

    Clean up main

--- mnist-classification/main.py
+++ mnist-classification/main.py
@@ -2,30 +2,9 @@ import numpy as np
 from perceptron import Perceptron
 from dataset import Dataset
 
-def main():
-    
-    mnist_data = Dataset.load_mnist()
-    '''
-    #perceptron = Perceptron(mnist_data["train_data"].shape[0], mnist_data["train_data"].shape[0], 0.001)
-    perceptron = Perceptron(784, 10, 0.001)
-    perceptron.train(mnist_data["train_data"], mnist_data["train_labels"], 0)
-    '''
-
-    for key in mnist_data:
-        print('{} has shape {}'.format(key, mnist_data[key].shape))
-    pass
-
-    '''
-    mnist_data = Dataset.load_mnist()['train_data'][0]
-    print('input data shape {}'.format(mnist_data.shape))
-    data = np.array([[0,0],[0,1],[1,0],[1,1]])
-    labels = np.array([[0],[1],[1],[1]])
-    perceptron = Perceptron(2, 1, 0.01)
-    perceptron.train(data, labels, 1)
-    '''
-
+def test_perceptron():
     #2 training samples, length 3
-    input_data = np.random.randint(-5, 5, size=(2, 3))
+    input_data = np.random.randint(-5, 5, size=(5, 3))
     input_labels = np.array(
         [
             [0,1],
@@ -37,6 +16,22 @@ def main():
     )
     perceptron = Perceptron(3, 2, 0.01)
     perceptron.train(input_data, input_labels, 1)
+
+
+def main():
+    epochs = 60
+    etas = [0.1, 0.01, 0.001]
+    mnist_data = Dataset.load_mnist()
+    perceptron = Perceptron(mnist_data["train_data"].shape[0],
+                            mnist_data["train_data"].shape[0],
+                            0.001)
+    perceptron = Perceptron(784, 10, 0.001)
+    epoch_accuracy = perceptron.train(mnist_data["train_data"], mnist_data["train_labels"], 5)
+    test_confusion_matrix = perceptron.test(mnist_data["test_data"], mnist_data["test_labels"])
+
+    for epoch in epoch_accuracy:
+        print('Epoch {}: %{}'.format(epoch, epoch_accuracy[epoch]))
+    print(test_confusion_matrix)
 
 if __name__ == '__main__':
     main()
