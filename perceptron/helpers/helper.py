84b481ad2583e659f2d38c9276cf2a7f4c3194e1
andy-keene <keene.andy.t@gmail.com>
2018-01-28 13:08:23 -0800

    Add matrix print func

--- perceptron/helpers/helper.py
+++ perceptron/helpers/helper.py
@@ -3,11 +3,29 @@ import time
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from tabulate import tabulate
+
+def print_matrix_as_table(file_name):
+    '''
+    Prints the confusion matrix of a json obj as a pretty table
+    Args:
+        file_name (string): file path of .json data, must contain 'confusion_matrix' key
+    Returns:
+        None
+    '''
+    headers = [str(x) for x in range(0,10)]
+    file_json = dict()
+
+    with open(file_name, 'r') as f:
+        file_json = json.load(f)
+    pretty_table = tabulate(np.array(file_json['confusion_matrix']), headers, tablefmt="grid")
+    print(pretty_table)
 
 def check_data_size(dataset):
     '''prints size of each value in the dataset'''
     for key in dataset:
         print('{} shape {}'.format(key, dataset[key].shape))
+
 def trim_dataset_for_test(mnist_data, size):
     '''trims dataset to given size (int)'''
     for key in mnist_data:
