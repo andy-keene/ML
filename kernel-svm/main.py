e55126ed4ddb68b6c9ca4cb756a7adeef0d9a9eb
andy-keene <keene.andy.t@gmail.com>
2018-03-08 17:36:33 -0800

    add preprocessor

--- kernel-svm/main.py
+++ kernel-svm/main.py
@@ -1,7 +1,8 @@
-from sklearn import datasets
-from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
 from preprocessor import Preprocessor
 
 train_file = './data/titanic_train.csv'
@@ -24,17 +25,23 @@ columns = [
 ]
 
 data_preprocessor = Preprocessor(train_file)
+processed_data = data_preprocessor.processed_df
+print(processed_data)
+
 data = data_preprocessor.get_matrix(['Pclass', 'Fare', 'Survived'])
 
 
-TEST_SIZE = 700
+TEST_SIZE = 800
 train, train_labels = data[:TEST_SIZE,:-1], data[:TEST_SIZE,-1]
 test, test_labels = data[TEST_SIZE:,:-1], data[TEST_SIZE:,-1]
 
 svm = SVC(kernel='rbf', C=15)
+scores = cross_val_score(svm, train, train_labels, cv=8)
+print('Accuracy: {} +/- {}'.format(scores.mean(), scores.std()))
+
+
 resp = svm.fit(train, train_labels)
 print(resp)
-
 predictions = svm.predict(test)
 print(predictions - test_labels)
 err = np.sum(abs(test_labels - predictions))
--- kernel-svm/preprocessor.py
+++ kernel-svm/preprocessor.py
@@ -1,30 +1,66 @@
 import pandas as pd
 import numpy as np
+pd.options.mode.chained_assignment = None
 
 class Preprocessor(object):
+    '''Reads and preprocesses the Titanic Dataset'''
+
     def __init__(self, filename):
-        self.df = pd.read_csv(filename)
-        pass
+        self.preprocessing_map = {
+            'Name': self._name,
+            'Sex': self._sex,
+            'Embarked': self._embarked,
+            'Ticket': self._zero,
+            'Cabin': self._zero
+            #ticket and cabin do not have preprocesing funcs
+        }
+        self.dataset_df = pd.read_csv(filename)
+        self.processed_df = self._preprocess(self.dataset_df)
 
     def get_matrix(self, cols):
         '''
         Args:
-            cols (list): subset of the dataframe to retrieve
+            cols (list): list of dataframe columns to retrieve
         Returns:
-            np.array: with columns matching cols
+            np.array: preprocessed data where np.array[:,n] == col[n]
         '''
-        _data = self.df[cols].as_matrix()
-        _data = np.nan_to_num(_data)
-        return _data
+        #_data = self.dataset_df[cols].as_matrix()
+        #_data = np.nan_to_num(_data)
+        #return _data
+        return np.nan_to_num(self.processed_df[cols].as_matrix())
 
-    def read_file(filename):
-        pass
+    def get_dataframe(self):
+        '''
+        Returns:
+            pd.dataframe: a copy of the original dataset
+        '''
+        return self.dataset_df.copy(deep=True)
 
-    def get_df(self):
-        ''' Returns a copy of the dataframe'''
-        return self.df.copy(deep=True)
+    def _preprocess(self, original_df):
+        '''
+        Args:
+            original_df (pd.dataframe): dataframe
+        Returns:
+            pd.dataframe: a preprocessed copy of original_df according to self.preprocessing_map rules
+        '''
+        _processed_df = original_df.copy(deep=True)
+        for field in self.preprocessing_map.keys():
+            _processed_df[field] = _processed_df[field].apply(self.preprocessing_map[field])
+        return _processed_df
 
-    def embarked_to_int(embarked):
+    # preprocessing functions
+    # TODO: move instance methods -> external | static ?...
+    def _sex(self, sex):
+        ''' passanger.sex -> (int)'''
+        if sex == 'female':
+            return 1
+        elif sex == 'male':
+            return 0
+        else:
+            return -1
+
+    def _embarked(self, embarked):
+        ''' passanger.embarked -> (int)'''         
         if embarked == 'S':
             return 0
         elif embarked == 'Q':
@@ -33,3 +69,21 @@ class Preprocessor(object):
             return 2
         else:
             return 3
+    
+    def _name(self, name):
+        ''' passanger.name -> (int)'''                 
+        if 'Sir.' in name:
+            return 5
+        elif 'Dr.' in name:
+            return 4
+        elif 'Mr.' in name:
+            return 3
+        elif 'Mrs.' in name:
+            return 2
+        elif 'Miss' in name:
+            return 1
+        else:
+            return 0
+    
+    def _zero(self, item):
+        return 0
